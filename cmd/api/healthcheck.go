66d2d45c551d8314d6f081af554b53c78717ddc8
Jesús Cabo Vázquez <jesusmcv19@gmail.com>
2024-11-18 22:16:06 +0100

    Chapter 3.5 page 61

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+)
+
+// Declare a handler which writes a plain-text response with information about the
+// application status, operating environment and version.
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	// Create a map which holds the information that we want to send in the response.
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
+	}
+	// Pass the map to the json.Marshal() function. This returns a []byte slice
+	// containing the encoded JSON. If there was an error, we log it and send the client
+	// a generic error message.
+	err := app.writeJSON(w, http.StatusOK, env, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jesusmcv19/greenlight
+
+go 1.22.8
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
